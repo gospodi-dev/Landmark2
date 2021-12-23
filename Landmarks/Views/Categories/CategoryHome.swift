3f82b8c397e2d99ed7ea47b7ec169ef2499cfbce
Gospodi <gospodijazzet@gmail.com>
2021-12-23 19:39:05 +0300

    Предоставим другой способ просмотра ориентиров (по категориям)

---
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,21 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Gospodi on 23.12.2021.
+//  Copyright © 2021 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
