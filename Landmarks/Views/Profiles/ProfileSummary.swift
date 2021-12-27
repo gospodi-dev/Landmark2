ae795d133a2eb9b7e2949834c667f30979edaa32
Gospodi <gospodijazzet@gmail.com>
2021-12-27 17:04:31 +0300

    Создаем представление, которое отображает некоторую базовую информацию о пользователе

---
+++ Landmarks/Views/Profiles/ProfileSummary.swift
@@ -0,0 +1,33 @@
+//
+//  ProfileSummary.swift
+//  Landmarks
+//
+//  Created by Gospodi on 27.12.2021.
+//  Copyright © 2021 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileSummary: View {
+    var profile: Profile
+
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 10) {
+                Text(profile.username)
+                    .bold()
+                    .font(.title)
+
+                Text("Notifications: \(profile.prefersNotifications ? "On": "Off" )")
+                Text("Seasonal Photos: \(profile.seasonalPhoto.rawValue)")
+                Text("Goal Date: ") + Text(profile.goalDate, style: .date)
+            }
+        }
+    }
+}
+
+struct ProfileSummary_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileSummary(profile: Profile.default)
+    }
+}
