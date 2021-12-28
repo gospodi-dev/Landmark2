04e334817cd68a1714e4a079fab347b57c34afe2
Gospodi <gospodijazzet@gmail.com>
2021-12-28 20:07:51 +0300

    Добавим новый файл представления SwiftUI с именем FeatureCard, который отображает изображение элемента ориентира.

---
+++ Landmarks/Views/PageView/FeatureCard.swift
@@ -0,0 +1,25 @@
+//
+//  FeatureCard.swift
+//  Landmarks
+//
+//  Created by Gospodi on 28.12.2021.
+//  Copyright © 2021 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct FeatureCard: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        landmark.featureImage?
+            .resizable()
+            .aspectRatio(3 / 2, contentMode: .fit)
+    }
+}
+
+struct FeatureCard_Previews: PreviewProvider {
+    static var previews: some View {
+        FeatureCard(landmark: ModelData().features[0])
+    }
+}
