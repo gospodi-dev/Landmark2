32029cd6af534d41e4e85b780988150c70ddc7fc
Gospodi <gospodijazzet@gmail.com>
2021-12-29 20:21:04 +0300

    Реализуем два требуемых метода UIPageViewController и DataSourceCoordinator

--- Landmarks/Views/PageView/PageViewController.swift
+++ Landmarks/Views/PageView/PageViewController.swift
@@ -26,18 +26,45 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
     
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
         pageViewController.setViewControllers(
-            [UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
+            [context.coordinator.controllers[0]], direction: .forward, animated: true)
     }
-    
-    class Coordinator: NSObject {
+
+    class Coordinator: NSObject, UIPageViewControllerDataSource {
         var parent: PageViewController
-        
+        var controllers = [UIViewController]()
+
         init(_ pageViewController: PageViewController) {
             parent = pageViewController
+            controllers = parent.pages.map { UIHostingController(rootView: $0) }
+        }
+
+        func pageViewController(
+            _ pageViewController: UIPageViewController,
+            viewControllerBefore viewController: UIViewController) -> UIViewController?
+        {
+            guard let index = controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            if index == 0 {
+                return controllers.last
+            }
+            return controllers[index - 1]
+        }
+
+        func pageViewController(
+            _ pageViewController: UIPageViewController,
+            viewControllerAfter viewController: UIViewController) -> UIViewController?
+        {
+            guard let index = controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            if index + 1 == controllers.count {
+                return controllers.first
+            }
+            return controllers[index + 1]
         }
     }
 }
-
 
 
 //  Контроллер представления страницы хранит массив Page экземпляров, который должен быть типа View. Это страницы, которые вы используете для прокрутки достопримечательностей.
