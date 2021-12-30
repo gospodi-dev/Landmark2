4cee155e2cea20fd6ed2eb3f62f76d71af70b7d4
Gospodi <gospodijazzet@gmail.com>
2021-12-30 20:23:21 +0300

    Создадим новый файл представления SwiftUI

---
+++ Landmarks/Views/PageView/PageControl.swift
@@ -0,0 +1,26 @@
+//
+//  PageControl.swift
+//  Landmarks
+//
+//  Created by Gospodi on 30.12.2021.
+//  Copyright © 2021 Apple. All rights reserved.
+//
+
+import SwiftUI
+import UIKit
+
+struct PageControl: UIViewRepresentable {
+    var numberOfPages: Int
+    @Binding var currentPage: Int
+
+    func makeUIView(context: Context) -> UIPageControl {
+        let control = UIPageControl()
+        control.numberOfPages = numberOfPages
+
+        return control
+    }
+
+    func updateUIView(_ uiView: UIPageControl, context: Context) {
+        uiView.currentPage = currentPage
+    }
+}
