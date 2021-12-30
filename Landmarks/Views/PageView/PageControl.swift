32f558cadb90b931d2732410b6bb0b3e40b41153
Gospodi <gospodijazzet@gmail.com>
2021-12-30 20:28:24 +0300

    Добавим метод для создания и возврата нового координатора

--- Landmarks/Views/PageView/PageControl.swift
+++ Landmarks/Views/PageView/PageControl.swift
@@ -13,6 +13,10 @@ struct PageControl: UIViewRepresentable {
     var numberOfPages: Int
     @Binding var currentPage: Int
 
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
     func makeUIView(context: Context) -> UIPageControl {
         let control = UIPageControl()
         control.numberOfPages = numberOfPages
@@ -22,5 +26,18 @@ struct PageControl: UIViewRepresentable {
 
     func updateUIView(_ uiView: UIPageControl, context: Context) {
         uiView.currentPage = currentPage
+    }
+
+    class Coordinator: NSObject {
+        var control: PageControl
+
+        init(_ control: PageControl) {
+            self.control = control
+        }
+
+        @objc
+        func updateCurrentPage(sender: UIPageControl) {
+            control.currentPage = sender.currentPage
+        }
     }
 }
