8480582c5f7182ee29096e2d626969768d405b9f
Gospodi <gospodijazzet@gmail.com>
2021-12-31 11:47:27 +0300

    Добавим шаблон приложения Watch для iOS

---
+++ WatchLandmarks WatchKit Extension/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  WatchLandmarks WatchKit Extension
+//
+//  Created by Gospodi on 31.12.2021.
+//  Copyright © 2021 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WatchLandmarks WatchKit Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  WatchLandmarks WatchKit Extension
+//
+//  Created by Gospodi on 31.12.2021.
+//  Copyright © 2021 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
